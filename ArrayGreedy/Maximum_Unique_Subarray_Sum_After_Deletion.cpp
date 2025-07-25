40a1699bc91e71f5f93405bab46e6225c2d96d1e
Akash Deep <93151820+AmbroseDAkash@users.noreply.github.com>
2025-07-25 18:20:27 +0530

    Add files via upload

---
+++ ArrayGreedy/Maximum_Unique_Subarray_Sum_After_Deletion.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_set<int>st;
+        int maxneg = INT_MIN;
+        int sum=0;
+
+        for(auto num:nums){
+            if(num<=0){
+                maxneg = max(maxneg,num);
+            }
+            else if(!st.count(num)){
+                sum+=num;
+                st.insert(num);
+            }
+        }
+        return sum==0? maxneg:sum;
+    }
+};
